67771e3d4e662e0fd17e54fd64a6108076d7556a
Waweru <developer.waweru@gmail.com>
2018-04-27 17:35:20 +0300

    Adding search component for making search requests

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,15 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { GithubUsersComponent } from './github-users/github-users.component';
 import { GitReposComponent } from './git-repos/git-repos.component';
+import { SearchComponent } from './search/search.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     GithubUsersComponent,
-    GitReposComponent
+    GitReposComponent,
+    SearchComponent
   ],
   imports: [
     BrowserModule
