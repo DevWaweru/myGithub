77ed636c1349075a1fcab75861e64f9ab264b1c8
Waweru <developer.waweru@gmail.com>
2018-04-28 00:49:28 +0300

    Pulling the router module and the navbar, and the components together

--- src/app/routing/routing.module.ts
+++ src/app/routing/routing.module.ts
@@ -4,11 +4,13 @@ import { RouterModule, Routes} from '@angular/router';
 
 import { GitReposComponent } from '../git-repos/git-repos.component';
 import { GithubUsersComponent } from '../github-users/github-users.component';
+import { NotFoundComponent } from '../not-found/not-found.component';
 
 const routes:Routes=[
   {path:"home",component:GithubUsersComponent},
   {path:"repos",component:GitReposComponent},
-  {path:"",redirectTo:"/home",pathMatch:"full"}
+  {path:"",redirectTo:"/home",pathMatch:"full"},
+  {path:'**',component:NotFoundComponent}
 ]
 
 @NgModule({
